87db42a70779ec2e469a701edcb6b66b331aa6f3
Chih Tsung Lu <lu001224@gmail.com>
2024-08-15 01:09:59 +0800

    fixed backend

--- backend/index.js
+++ backend/index.js
@@ -276,6 +276,9 @@ async function generateOverallSummary(client, matches) {
 }
 
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
 
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'));
